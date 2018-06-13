73e364b029ab6b1b9a3837329364b6ae1eb4441f
Rebecca Souza <rebeccacsouza@gmail.com>
2018-06-13 01:34:13 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,7 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-puts "Hi! You're being invited to a party. What is your name?"
+puts "Hi! You've been invited to a party! What is your name?"
 guest_name = gets.chomp
 puts "Which party are you attending?"
 party_name = gets.chomp
